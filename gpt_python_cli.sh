adedb4f204b0c0f0c47dcf3ea3c923d99d6a1d94
bradjohnl <lucabraggion@pm.me>
2023-06-20 16:27:42 +0200

    remove requirements and fix script

--- gpt_python_cli.sh
+++ gpt_python_cli.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Get the directory of the current script
+DIR="$(dirname "$(readlink -f "$0")")"
+
 # activate the environment
 source activate gpt_python_cli
 
 # execute the script with all arguments passed to this script
-python main.py "$@"
+python "$DIR/main.py" "$@"
 
 # deactivate the environment
 conda deactivate
+
